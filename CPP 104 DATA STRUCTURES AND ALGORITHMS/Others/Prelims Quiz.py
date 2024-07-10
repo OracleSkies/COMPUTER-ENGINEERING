8c7fb696404813b401b7390ec6b0cb6f3f004316
OracleSkies <153785033+OracleSkies@users.noreply.github.com>
2024-07-10 20:09:48 +0800

    Create Prelims Quiz.py

---
+++ CPP 104 DATA STRUCTURES AND ALGORITHMS/Others/Prelims Quiz.py
@@ -0,0 +1,18 @@
+def evaluate(exp):
+    stack = []
+    for char in exp:
+        if char.isdigit():
+            stack.append(int(char))
+        else:
+            val1 = stack.pop()
+            val2 = stack.pop()
+            if char == '+':
+                stack.append(val2 + val1)
+            elif char == '-':
+                stack.append(val2 - val1)
+            elif char == '*':
+                stack.append(val2 * val1)
+            elif char == '/':
+                stack.append(val2 / val1)
+    return stack.pop()
+print(evaluate("231*+8-"))
