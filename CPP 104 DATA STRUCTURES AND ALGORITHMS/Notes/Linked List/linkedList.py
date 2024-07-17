de1646e0f42c99d375ac8aa371a3ae7aaf149ac2
Jayjay Donasco <153785033+OracleSkies@users.noreply.github.com>
2024-07-17 13:30:03 +0800

    LinkedList.py update

--- CPP 104 DATA STRUCTURES AND ALGORITHMS/Notes/Linked List/linkedList.py
+++ CPP 104 DATA STRUCTURES AND ALGORITHMS/Notes/Linked List/linkedList.py
@@ -1,46 +1,100 @@
+class Node:
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
+
 class LinkedList:
     def __init__(self):
-        self.linkedList = []
-    def insertAtBegining(self, data):
-        pass #adds a new node with the given data at the beginnng of the linked list.
+        self.head = None
 
-    def insertAtEnd(self,data):
-        pass #adds a new node with the given data at the end of the linked list
+    def is_empty(self):
+        return self.head is None
 
-    def insertAfterNode(self,prevNode,data):
-        pass #adds a new node with the given data after the specified previous node.
+    def append(self, data):
+        new_node = Node(data)
+        if self.is_empty():
+            self.head = new_node
+            return
+        last_node = self.head
+        while last_node.next:
+            last_node = last_node.next
+        last_node.next = new_node
 
-    def deleteNode(self,data):
-        pass #removes the first occurrence of a node containing the specified data.
+    def prepend(self, data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
 
-    def deleteAtPosition(self, position):
-        pass #removes the node at the specified position in the linked list
+    def insert_after_node(self, prev_node, data):
+        if not prev_node:
+            print("Previous node is not in the list")
+            return
+        new_node = Node(data)
+        new_node.next = prev_node.next
+        prev_node.next = new_node
 
-    def deleteFirst(self):
-        pass #removes the first node in the linked list
+    def delete_node(self, key):
+        temp = self.head
+        if temp and temp.data == key:
+            self.head = temp.next
+            temp = None
+            return
+        prev = None
+        while temp and temp.data != key:
+            prev = temp
+            temp = temp.next
+        if temp is None:
+            return
+        prev.next = temp.next
+        temp = None
 
-    def deleteLast(self):
-        pass #removes the first node in the linked list
+    def delete_node_at_position(self, position):
+        if self.head is None:
+            return
+        temp = self.head
+        if position == 0:
+            self.head = temp.next
+            temp = None
+            return
+        for _ in range(position - 1):
+            temp = temp.next
+            if temp is None:
+                return
+        if temp is None or temp.next is None:
+            return
+        next_node = temp.next.next
+        temp.next = None
+        temp.next = next_node
 
-    def printList(self):
-        pass #prints all the elements in the linked list
+    def length(self):
+        count = 0
+        current = self.head
+        while current:
+            count += 1
+            current = current.next
+        return count
 
-    def getLength(self):
-        pass #returns the number of node in the linked list
-
-    def search(self):
-        pass #searches for a containing the specified data and returns True if found, otherwise False.
+    def search(self, key):
+        current = self.head
+        while current:
+            if current.data == key:
+                return True
+            current = current.next
+        return False
 
     def reverse(self):
-        pass #reverses the order of nodes in the linked list
+        prev = None
+        current = self.head
+        while current:
+            next_node = current.next
+            current.next = prev
+            prev = current
+            current = next_node
+        self.head = prev
 
-    def isEmpty(self):
-        pass 
-
-    def getNthNode(self):
-        pass
-
-    def size(self):
-        pass
-
-
+    def print_list(self):
+        current = self.head
+        while current:
+            print(current.data, end=" -> ")
+            current = current.next
+        print("None")
